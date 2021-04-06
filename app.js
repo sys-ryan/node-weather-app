c3579cf51064a80d1f499fefc0d36dededcb699f
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-04-06 22:13:52 +0900

    refactor: geocode

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
 const request = require("request");
+const geocode = require("./utils/geocode");
 require("dotenv").config();
 
 const weatherstack_api_key = process.env.WEATHERSTACK_API_KEY;
 const weatherstack_url = `http://api.weatherstack.com/current?access_key=${weatherstack_api_key}&query=37.8267,-122.4233`;
 
-request({ url: weatherstack_url, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to weather service!");
-  } else if (response.body.error) {
-    console.log("Unable to find location");
-  } else {
-    // const data = JSON.parse(response.body);
-    const data = response.body; // { json: true }
-    const current = response.body.current;
-    const message = `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out.
-      It feels like ${current.feelslike} degrees out.`;
-    console.log(message);
-  }
-});
+// request({ url: weatherstack_url, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("Unable to connect to weather service!");
+//   } else if (response.body.error) {
+//     console.log("Unable to find location");
+//   } else {
+//     // const data = JSON.parse(response.body);
+//     const data = response.body; // { json: true }
+//     const current = response.body.current;
+//     const message = `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out.
+//       It feels like ${current.feelslike} degrees out.`;
+//     console.log(message);
+//   }
+// });
 
-const mapbox_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/Washington.json?limit=1&access_token=${process.env.MAPBOX_API_KEY}`;
-
-request({ url: mapbox_url, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to geocode service!");
-  } else if (response.body.features.length === 0) {
-    console.log("Unable to find location");
-  } else {
-    const latitude = response.body.features[0].center[1];
-    const longitude = response.body.features[0].center[0];
-    console.log(latitude, longitude);
-  }
+geocode("Boston", (error, data) => {
+  console.log("error", error);
+  console.log("data", data);
 });
