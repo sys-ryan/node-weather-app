414d24d3fcda12a8eb65b76821598f7ade137793
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-04-06 22:30:17 +0900

    refactor: forecast
    - create utils/forecast.js
    - modifdy app.js

---
+++ utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const weatherstack_api_key = process.env.WEATHERSTACK_API_KEY;
+  const weatherstack_url = `http://api.weatherstack.com/current?access_key=${weatherstack_api_key}&query=37.8267,-122.4233`;
+
+  request({ url: weatherstack_url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (response.body.error) {
+      callback("Unable to find location");
+    } else {
+      const current = response.body.current;
+      const message = `${current.weather_descriptions[0]}. Iti s currently ${current.temperature} degrees out.
+      It feels like ${current.feelslike} degrees out.`;
+      callback(undefined, message);
+    }
+  });
+};
+
+module.exports = forecast;
