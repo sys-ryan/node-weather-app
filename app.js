887084ab287dacd6df33d7981894622e9d6e1d7f
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-04-06 21:31:20 +0900

    feat: Error handling for weatherstack API

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const weatherstack_api_key = process.env.WEATHERSTACK_API_KEY;
 const weatherstack_url = `http://api.weatherstack.com/current?access_key=${weatherstack_api_key}&query=37.8267,-122.4233`;
 
 request({ url: weatherstack_url, json: true }, (error, response) => {
-  // const data = JSON.parse(response.body);
-  const data = response.body; // { json: true }
-  const current = response.body.current;
-  const message = `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out.
-    It feels like ${current.feelslike} degrees out.`;
-
-  console.log(message);
+  if (error) {
+    console.log("Unable to connect to weather service!");
+  } else if (response.body.error) {
+    console.log("Unable to find location");
+  } else {
+    // const data = JSON.parse(response.body);
+    const data = response.body; // { json: true }
+    const current = response.body.current;
+    const message = `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out.
+      It feels like ${current.feelslike} degrees out.`;
+    console.log(message);
+  }
 });
 
 //
