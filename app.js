21840da081a0b004041e7e1d7e9661d765e7b779
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-04-07 23:09:09 +0900

    refactor: object destructuring
    - app.js
    - utils/forecast.js
    - utils/geocode.js

--- app.js
+++ app.js
@@ -3,27 +3,21 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 require("dotenv").config();
 
-// request({ url: weatherstack_url, json: true }, (error, response) => {
-//   if (error) {
-//     console.log("Unable to connect to weather service!");
-//   } else if (response.body.error) {
-//     console.log("Unable to find location");
-//   } else {
-//     // const data = JSON.parse(response.body);
-//     const data = response.body; // { json: true }
-//     const current = response.body.current;
-//     const message = `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out.
-//       It feels like ${current.feelslike} degrees out.`;
-//     console.log(message);
-//   }
-// });
+const location = process.argv[2];
+if (location) {
+  geocode(location, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return console.log(error);
+    }
 
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
-
-// geocode("Boston", (error, data) => {
-//   console.log("error", error);
-//   console.log("data", data);
-// });
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(location);
+      console.log(forecastData);
+    });
+  });
+} else {
+  console.log("Location info is required");
+}
